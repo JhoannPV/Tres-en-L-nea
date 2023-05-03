8720e6cb59868f6100e96600de101a6923ae97b9
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2023-05-03 13:13:21 -0500

    calcular color

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,9 +7,9 @@ function Board({ xIsNext, squares, onPlay }) {
     }
     const nextSquares = squares.slice();
     if (xIsNext) {
-      nextSquares[i] = 'X';
+      nextSquares[i] = "X";
     } else {
-      nextSquares[i] = 'O';
+      nextSquares[i] = "O";
     }
     onPlay(nextSquares);
   }
@@ -17,27 +17,27 @@ function Board({ xIsNext, squares, onPlay }) {
   const empate = calcularEmpate(squares, winner);
   let status;
   if (winner) {
-    status = 'Ganador: ' + winner;
+    status = "Ganador: " + winner.winner;
   } else {
-    status = 'Siguiente Jugador: ' + (xIsNext ? 'X' : 'O');
+    status = "Siguiente Jugador: " + (xIsNext ? "X" : "O");
   }
-  if(empate){
-    status = 'Empate';
+  if (empate) {
+    status = "Empate";
   }
 
-  const squareColors = ['square','square2'];
+  const squareColors = ["square", "square2"];
   let colorButton;
 
-  function cambiarColor(i){
-    if(winner){
-      colorButton = calculateNumButtonColor(squares);
-      const [a,b,c] = colorButton;
-      if(i===a || i===b || i===c){
+  function cambiarColor(i) {
+    if (winner) {
+      colorButton = winner.line;
+      const [a, b, c] = colorButton;
+      if (i === a || i === b || i === c) {
         return squareColors[1];
-      }else{
+      } else {
         return squareColors[0];
       }
-    }else{
+    } else {
       return squareColors[0];
     }
   }
@@ -45,38 +45,76 @@ function Board({ xIsNext, squares, onPlay }) {
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} color={cambiarColor(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} color={cambiarColor(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} color={cambiarColor(2)}/>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+          color={cambiarColor(0)}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+          color={cambiarColor(1)}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+          color={cambiarColor(2)}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} color={cambiarColor(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} color={cambiarColor(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} color={cambiarColor(5)}/>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+          color={cambiarColor(3)}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+          color={cambiarColor(4)}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+          color={cambiarColor(5)}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} color={cambiarColor(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} color={cambiarColor(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} color={cambiarColor(8)}/>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+          color={cambiarColor(6)}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+          color={cambiarColor(7)}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+          color={cambiarColor(8)}
+        />
       </div>
     </>
   );
 }
-function calcularEmpate(squares, winner){
-  let arreglo=squares.slice();
-  let numMoves = arreglo.map((el)=>el==='X' || el ==='O'? 1 : 0).reduce((el,item)=> el+item,0);
-  if(numMoves===8){
-    let copiaArreglo=arreglo.map((el)=> el===null? 'X' : el);
+function calcularEmpate(squares, winner) {
+  let arreglo = squares.slice();
+  let numMoves = arreglo
+    .map((el) => (el === "X" || el === "O" ? 1 : 0))
+    .reduce((el, item) => el + item, 0);
+  if (numMoves === 8) {
+    let copiaArreglo = arreglo.map((el) => (el === null ? "X" : el));
     const winner2 = calculateWinner(copiaArreglo);
-    if(!winner2){
+    if (!winner2) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
-  if(numMoves===9 && !winner){
+  if (numMoves === 9 && !winner) {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
@@ -95,30 +133,10 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], line: lines[i] };
     }
   }
   return null;
 }
-function calculateNumButtonColor(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return lines[i];
-    }
-  }
-  return null;
-}
-
 
 export default Board;
